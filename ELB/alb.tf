91e6a6b86366a9e9965caa8e3c46b30f1ee94f9e
Ayomide Ajayi <lancerajayi@yahoo.com>
2024-10-30 03:06:48 +0000

    Redis and ALB updates

--- ELB/alb.tf
+++ ELB/alb.tf
@@ -1,11 +1,11 @@
 #Create Application Load Balancer
-resource "aws_lb" "my-alb" {
+resource "aws_lb" "app-lb" {
   name                       = "MY-ALB"
   internal                   = false
   load_balancer_type         = "application"
   vpc_id                     = aws_vpc.main.id
   subnets                    = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-  security_groups            = [aws_security_group.sg_node.id]
+  security_groups            = [aws_security_group.sg_node.id] #module reference required?
   enable_deletion_protection = false
 
   tags = {
@@ -16,11 +16,11 @@ resource "aws_lb" "my-alb" {
 
 #Create an ALB Target Groups for Frontend Nodes 1 and Frontend Node 2
 
-resource "aws_lb_target_group" "my-tg-frontend" {
-  name     = "target-group-frontend"
+resource "aws_lb_target_group" "frontend-tg" {
+  name     = "Frontend-Target-Group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id #var.vpc_id    
+  vpc_id   = aws_vpc.main.id # data refrence required?    
 
   target_group_health {
     dns_failover {
@@ -37,7 +37,7 @@ resource "aws_lb_target_group" "my-tg-frontend" {
 #Create Listener
 
 resource "aws_lb_listener" "alb-ls" {
-  load_balancer_arn = aws_lb.my-alb.arn
+  load_balancer_arn = aws_lb.app-lb.arn
   port              = "80"
   protocol          = "HTTP"
   #ssl_policy        = "ELBSecurityPolicy-2016-08"
@@ -45,19 +45,19 @@ resource "aws_lb_listener" "alb-ls" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.my-tg-frontend.arn
+    target_group_arn = aws_lb_target_group.frontend-tg.arn 
   }
 }
 
 #Add the ALB Target Group Attachment for Frontend Node 1 and Frontend Node 2
 
 resource "aws_lb_target_group_attachment" "tg_attachemnet_frontend" {
-  target_group_arn = aws_lb_target_group.my-tg-frontend.arn
+  target_group_arn = aws_lb_target_group.frontend-tg.arn
   target_id        = [module.alb-ec2-sg.first-frontend-machine.aws_instance.app_node, module.alb-ec2-sg.second-frontend-machine.aws_instance.app_node]
   port             = 80
 }
 
-resource "aws_vpc" "main" {
+/* resource "aws_vpc" "main" {
   cidr_block           = "192.168.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -89,4 +89,4 @@ resource "aws_subnet" "public_subnet_2" {
   tags = {
     Name = "PubSub 2"
   }
-}
+} */
--- Redis.tf
+++ Redis.tf
@@ -1,13 +1,13 @@
-#Steps to create an Amazon ElastiCache for Redis
+#Create an Amazon ElastiCache for Redis
 
-#step 1: create vpc and subnets 
-#Step 2: Create a subnet group to host the ElastiCache cluster
-#Not required since it is a Single Master Node to be deployed in a private subnet in eu-west-2b
+    #Use main VPC ID and subnets 
 
-#Step 3: create a security group to access the ElastiCache cluster
+    #A Single Master Node to be deployed in a private subnet in eu-west-2b
 
-resource "aws_security_group" "elasticache_sg" {
-    name = "elasticache-sg"
+#Create a security group to access the ElastiCache cluster
+
+resource "aws_security_group" "redis_sg" {
+    name = "REDIS-SG"
     description = "Allows inbound and outbound access from the Amazon elasticache cluster"
     
     ingress {
@@ -17,6 +17,7 @@ resource "aws_security_group" "elasticache_sg" {
         cidr_blocks = ["192.168.3.0/24"]
         description = "Enable access from an amazon EC2 instance in the VPC"
     }
+
 egress {
     from_port = "0"
     to_port = "0"
@@ -24,21 +25,23 @@ egress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Enable access to the Elasticache cluster"
 }
-vpc_id = "vpc-0a37aa0f53f23e78c" #data.aws_vpc.main.id 
+vpc_id = data.aws_vpc.main.id 
 
 }
 
-resource "aws_elasticache_subnet_group" "redis_private_subnet_group" {
-  name = "Redis-Private_subnet_group"
+#Create a subnet group to host the ElastiCache cluster
+
+resource "aws_elasticache_subnet_group" "redis_psg" {
+  name = "Redis-Private-Subnet-Group"
   subnet_ids = aws_subnet.private_subnet_2 
  
  }
 
-#step 4: Create the Amazon ElastiCache for redis cluster
+#Create the Amazon ElastiCache for redis cluster
 
 resource "aws_elasticache_replication_group" "redis-node" {
     automatic_failover_enabled = true
-    subnet_group_name = aws_elasticache_subnet_group.redis_private_subnet_group.id
+    subnet_group_name = aws_elasticache_subnet_group.redis_psg.id
     preferred_cache_cluster_azs = ["eu-west-2b"]
     replication_group_id = "tf-rep-group-1"              
     description = "ElastiCache cluster for  redis-node"
@@ -48,7 +51,6 @@ resource "aws_elasticache_replication_group" "redis-node" {
     multi_az_enabled = false
     num_node_groups = 1
     replicas_per_node_group = 2
-    security_group_ids = [aws_security_group.elasticache_sg.id]
+    security_group_ids = [aws_security_group.redis_sg.id]
     }
-   
    
