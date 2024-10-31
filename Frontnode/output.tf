20d063aae16969532e38fab3e55f05900ac0ef1b
Ayomide Ajayi <lancerajayi@yahoo.com>
2024-10-31 18:10:36 +0000

    redis fix

--- Frontnode/output.tf
+++ Frontnode/output.tf
@@ -1,8 +1,8 @@
 /*  
 output "first-frontend-machine" {
-  value = module.first-frontend-machine
+  value = module.first-frontend-machine.public_dns
 }
 output "second-frontend-machine" {
-  value = module.second-frontend-machine
+  value = module.second-frontend-machine.public_dns
 }
  */
