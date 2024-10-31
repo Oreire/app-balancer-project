a24a43a531943b5056d9e6e42971cbe2162710b8
Ayomide Ajayi <lancerajayi@yahoo.com>
2024-10-31 01:52:39 +0000

    Fixing Redis Cache

--- Redis.tf
+++ Redis.tf
@@ -1,8 +1,6 @@
 #Create an Amazon ElastiCache for Redis
 
-    #Use main VPC ID and subnets 
-
-    #A Single Master Node to be deployed in a private subnet in eu-west-2b
+#Use VPC outputs as Data sources to provision a Single Master Node to be deployed in a private subnet in eu-west-2b
 
 #Create a security group to access the ElastiCache cluster
 
@@ -25,17 +23,15 @@ egress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Enable access to the Elasticache cluster"
 }
-vpc_id = data.aws_vpc.main.id 
-
+vpc_id         = data.terraform_remote_state.remote.outputs.vpc_id
 }
 
 #Create a subnet group to host the ElastiCache cluster
 
 resource "aws_elasticache_subnet_group" "redis_psg" {
   name = "Redis-Private-Subnet-Group"
-  subnet_ids = aws_subnet.private_subnet_2 
- 
- }
+  subnet_ids = [data.terraform_remote_state.remote.outputs.private_subnet_1, data.terraform_remote_state.remote.outputs.private_subnet_2]
+  }
 
 #Create the Amazon ElastiCache for redis cluster
 
@@ -44,9 +40,10 @@ resource "aws_elasticache_replication_group" "redis-node" {
     subnet_group_name = aws_elasticache_subnet_group.redis_psg.id
     preferred_cache_cluster_azs = ["eu-west-2b"]
     replication_group_id = "tf-rep-group-1"              
-    description = "ElastiCache cluster for  redis-node"
+    description = "ElastiCache cluster for Redis-node"
     node_type = "cache.t3.micro"
-    parameter_group_name = "default.redis3.2"
+    engine_version = 7.1
+    parameter_group_name = "default.redis7"
     port = 6379
     multi_az_enabled = false
     num_node_groups = 1
